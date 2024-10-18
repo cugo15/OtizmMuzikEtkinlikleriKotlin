6ffa2787aa5a3585e28ae344bc2db89322fe599f
Cumhur Günay <cugo2435@gmail.com>
2024-10-18 16:01:43 +0300

    Created SesCategory enum class to categorize sound types

---
+++ app/src/main/java/com/aecg/oyunvemuzikae/Sesler/SesCategory.kt
@@ -0,0 +1,11 @@
+package com.aecg.oyunvemuzikae.Sesler
+
+enum class SesCategory(val displayName: String) {
+    HAYVAN("Hayvan Sesleri"),
+    INSAN("İnsan Sesleri"),
+    ENSTRUMAN("Enstrüman Sesleri"),
+    DOGA("Doğa Sesleri"),
+    ARAC("Araç Sesleri"),
+    SAYI("Sayılar"),
+    SEKIL("Geometrik Şekiller");
+}
