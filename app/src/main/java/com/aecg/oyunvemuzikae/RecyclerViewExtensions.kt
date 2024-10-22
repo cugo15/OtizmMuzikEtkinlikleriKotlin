25df55de21aeccf0319cf43b28d5e76650b6e01f
Cumhur Günay <cugo2435@gmail.com>
2024-10-22 14:36:08 +0300

    Added extension function to set background on View using Glide

--- app/src/main/java/com/aecg/oyunvemuzikae/RecyclerViewExtensions.kt
+++ app/src/main/java/com/aecg/oyunvemuzikae/RecyclerViewExtensions.kt
@@ -1,7 +1,13 @@
 package com.aecg.oyunvemuzikae
 
+import android.content.Context
+import android.graphics.drawable.Drawable
+import android.view.View
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
+import com.bumptech.glide.Glide
+import com.bumptech.glide.request.target.CustomTarget
+import com.bumptech.glide.request.transition.Transition
 
 fun RecyclerView.scrollInDirection(direction: Int) {
     val layoutManager = this.layoutManager as LinearLayoutManager
@@ -15,4 +21,24 @@ fun RecyclerView.scrollInDirection(direction: Int) {
         else -> return
     }
     this.smoothScrollToPosition(newPosition)
+}
+
+fun View.loadLayoutBackgroundWithGlide(context: Context, resourceId: Int, defaultDrawableId: Int) {
+    Glide.with(context)
+        .load(resourceId)
+        .into(object : CustomTarget<Drawable>() {
+            override fun onResourceReady(resource: Drawable, transition: Transition<in Drawable>?) {
+                // Yüklenen Drawable'i arka plana set et
+                this@loadLayoutBackgroundWithGlide.background = resource
+            }
+
+            override fun onLoadCleared(placeholder: Drawable?) {
+                // Yükleme iptal edildiğinde yapılacak işlemler
+            }
+
+            override fun onLoadFailed(errorDrawable: Drawable?) {
+                // Yükleme başarısız olursa varsayılan arka plan ayarla
+                this@loadLayoutBackgroundWithGlide.setBackgroundResource(defaultDrawableId) // varsayılan arka plan
+            }
+        })
 }
