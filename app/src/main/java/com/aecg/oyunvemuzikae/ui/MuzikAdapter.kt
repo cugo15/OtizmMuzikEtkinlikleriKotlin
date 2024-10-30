4dbb2e674bfb0bfbec841ef434e2c041ace85d20
Cumhur Günay <cugo2435@gmail.com>
2024-10-30 14:56:57 +0300

    RecyclerView Adapter created

--- app/src/main/java/com/aecg/oyunvemuzikae/ui/MuzikAdapter.kt
+++ app/src/main/java/com/aecg/oyunvemuzikae/ui/MuzikAdapter.kt
@@ -1,4 +1,32 @@
 package com.aecg.oyunvemuzikae.ui
 
-class MuzikAdapter {
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.recyclerview.widget.RecyclerView
+import com.aecg.oyunvemuzikae.databinding.MenuItemBinding
+
+class MuzikAdapter(
+    private val muzikList: ArrayList<MuzikModel>, // SesModel nesnelerinin bulunduğu liste
+    private val onItemClick: (String,MuzikType) -> Unit, // Tıklama olayı için callback
+    private val onItemAnimate: (View) -> Unit // Animasyon tetiklemek için callback
+) : RecyclerView.Adapter<MuzikViewHolder>() {
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): MuzikViewHolder {
+        val inflater = LayoutInflater.from(parent.context)
+        val binding = MenuItemBinding.inflate(inflater, parent, false)
+        return MuzikViewHolder(binding)
+    }
+    // Adapter'deki toplam item sayısını döndür
+    override fun getItemCount(): Int {
+        return muzikList.size
+    }
+
+    // Her bir ViewHolder için veriyi bağlamak için çağrılır
+    override fun onBindViewHolder(holder: MuzikViewHolder, position: Int) {
+        // Mevcut pozisyondaki SesModel nesnesini al
+        val currentSesModel = muzikList[position]
+        // ViewHolder'ı bağla ve gerekli callback'leri geçir
+        holder.bind(currentSesModel, onItemClick, onItemAnimate)
+    }
 }
--- app/src/main/java/com/aecg/oyunvemuzikae/ui/MuzikViewHolder.kt
+++ app/src/main/java/com/aecg/oyunvemuzikae/ui/MuzikViewHolder.kt
@@ -1,4 +1,32 @@
 package com.aecg.oyunvemuzikae.ui
 
-class MuzikViewHolder {
+import android.view.View
+import androidx.recyclerview.widget.RecyclerView
+import com.aecg.oyunvemuzikae.databinding.MenuItemBinding
+import com.bumptech.glide.Glide
+
+class MuzikViewHolder(val binding: MenuItemBinding) : RecyclerView.ViewHolder(binding.root) {
+
+    // Bind fonksiyonu, bir SesModel nesnesini alır ve gerekli verileri UI bileşenlerine atar
+    inline fun bind(
+        muzikModel: MuzikModel,
+        crossinline onClickListener: (String,MuzikType) -> Unit,
+        crossinline onItemAnimated: (View) -> Unit
+    ) {
+        // Ses modelinin görselini ata
+        Glide.with(binding.imgMenu.context)
+            .load(muzikModel.imageResourceId)
+            .into(binding.imgMenu)
+
+        // Ses modelinin adını TextView'a ata
+        binding.txtMenu.text = muzikModel.muzikName
+
+        // Tıklama olayının tetiklenmesi
+        binding.root.setOnClickListener {
+            // Tıklandığında, ses kaynağı ID'sini dışarıya ilet
+            onClickListener(muzikModel.muzikUri,muzikModel.muzikType)
+            // Tıklandığında, animasyon için kullanılan view'i dışarıya ilet
+            onItemAnimated(binding.cardMenu)
+        }
+    }
 }
