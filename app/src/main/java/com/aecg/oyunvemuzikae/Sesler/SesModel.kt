678eab3de4d7b02b98dc92df2e2538bb30d2b62c
Cumhur Günay <cugo2435@gmail.com>
2024-10-30 14:43:52 +0300

    Used Annotations

--- app/src/main/java/com/aecg/oyunvemuzikae/Sesler/SesModel.kt
+++ app/src/main/java/com/aecg/oyunvemuzikae/Sesler/SesModel.kt
@@ -1,32 +1,12 @@
 package com.aecg.oyunvemuzikae.Sesler
 
-import android.os.Parcel
 import android.os.Parcelable
+import kotlinx.parcelize.Parcelize
 
+@Parcelize
 data class SesModel(
     val sesName: String,
     val type: SesType, // type enum olarak tanımlı
     val sesResourceId: Int, // raw klasöründeki ses dosyasını temsil eden ID
     val imageResourceId: Int
-) : Parcelable {
-    constructor(parcel: Parcel) : this(
-        parcel.readString() ?: "", // sesName
-        SesType.valueOf(parcel.readString() ?: SesType.HAYVAN.name), // type
-        parcel.readInt(), // sesResourceId
-        parcel.readInt() // imageResourceId
-    )
-
-    override fun writeToParcel(parcel: Parcel, flags: Int) {
-        parcel.writeString(sesName) // sesName
-        parcel.writeString(type.name) // category'yi yaz
-        parcel.writeInt(sesResourceId) // sesResourceId
-        parcel.writeInt(imageResourceId) // imageResourceId
-    }
-
-    override fun describeContents(): Int = 0
-
-    companion object CREATOR : Parcelable.Creator<SesModel> {
-        override fun createFromParcel(parcel: Parcel): SesModel = SesModel(parcel)
-        override fun newArray(size: Int): Array<SesModel?> = arrayOfNulls(size)
-    }
-}
+) : Parcelable
