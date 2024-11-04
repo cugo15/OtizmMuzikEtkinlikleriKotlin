1f60695bc41023fe32d23f658d1fd3d8155c8a0f
Cumhur Günay <cugo2435@gmail.com>
2024-11-04 12:45:59 +0300

    Added muzik fragments

--- app/src/main/java/com/aecg/oyunvemuzikae/ui/MuzikFragment.kt
+++ app/src/main/java/com/aecg/oyunvemuzikae/ui/MuzikFragment.kt
@@ -1,17 +1,31 @@
 package com.aecg.oyunvemuzikae.ui
 
+import android.app.Dialog
+import android.media.MediaPlayer
+import android.net.Uri
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.view.animation.Animation
+import android.view.animation.AnimationUtils
+import androidx.navigation.fragment.findNavController
+
+import androidx.recyclerview.widget.LinearLayoutManager
+import com.aecg.oyunvemuzikae.R
 import com.aecg.oyunvemuzikae.databinding.FragmentMuzikBinding
+
 
 
 class MuzikFragment : Fragment() {
 
     private var _binding: FragmentMuzikBinding? = null
     private val binding get() = _binding!!
+
+    private lateinit var mediaPlayer: MediaPlayer
+    private lateinit var muzikList: ArrayList<MuzikModel>
+    private lateinit var animationzoom: Animation
 
     override fun onCreateView(
         inflater: LayoutInflater,
@@ -20,11 +34,60 @@ class MuzikFragment : Fragment() {
     ): View {
         _binding = FragmentMuzikBinding.inflate(inflater, container, false)
         val view = binding.root
+        animationzoom = AnimationUtils.loadAnimation(requireContext(), R.anim.zoom_inshort)
+
+        muzikList = MuzikFragmentArgs.fromBundle(requireArguments()).muzikList.toCollection(ArrayList())
+        setupRecyclerView(muzikList)
+
+
         return view
     }
 
+    private fun setupRecyclerView(muzikList: ArrayList<MuzikModel>) {
+        // RecyclerView'e yatay (horizontal) LinearLayoutManager ata
+        binding.rvMuzikler.layoutManager = LinearLayoutManager(requireContext(), LinearLayoutManager.HORIZONTAL, false)
+        // SesAdapter'ı RecyclerView'e ata
+        binding.rvMuzikler.adapter = MuzikAdapter(muzikList, { uri,type ->
+            if (type== MuzikType.MUZIK){
+                playSoundByUri(uri)
+            }else{
+                openVideo(uri)
+            }
+        }) { view ->
+            // Animasyonu başlat
+            view.startAnimation(animationzoom)
+        }
+    }
+
+    private fun playSoundByUri(uri: String) {
+        // Önce mevcut MediaPlayer nesnesini serbest bırak
+        if (::mediaPlayer.isInitialized) {
+            mediaPlayer.release()
+        }
+        // Yeni MediaPlayer nesnesi oluştur ve URI ayarla
+        mediaPlayer = MediaPlayer().apply {
+            setDataSource(requireContext(), Uri.parse(uri))
+            prepare() // Asenkron yükleme gerekiyorsa prepareAsync() kullanabilirsiniz
+            start()
+        }
+        // Ses tamamlandığında MediaPlayer'ı serbest bırak
+        mediaPlayer.setOnCompletionListener {
+            it.release()
+        }
+    }
+    private fun openVideo(videoId: String) {
+        val action = MuzikFragmentDirections.actionMuzikFragmentToWebViewDialogFragment(videoId)
+        findNavController().navigate(action)
+    }
+
+
+
     override fun onDestroyView() {
         super.onDestroyView()
+        if (::mediaPlayer.isInitialized) {
+            mediaPlayer.release()
+        }
+        muzikList.clear()
         _binding = null
     }
 }
---
+++ app/src/main/java/com/aecg/oyunvemuzikae/ui/WebViewDialogFragment.kt
@@ -0,0 +1,87 @@
+package com.aecg.oyunvemuzikae.ui
+
+import android.annotation.SuppressLint
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.webkit.WebResourceRequest
+import android.webkit.WebView
+import android.webkit.WebViewClient
+import androidx.fragment.app.Fragment
+import com.aecg.oyunvemuzikae.databinding.FragmentWebViewDialogBinding
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
+
+class WebViewDialogFragment : Fragment() {
+
+    private var _binding: FragmentWebViewDialogBinding? = null
+    private val binding get() = _binding!!
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+        _binding = FragmentWebViewDialogBinding.inflate(inflater, container, false)
+        val videoId = WebViewDialogFragmentArgs.fromBundle(requireArguments()).videoId
+
+        setupWebView()
+        loadYouTubeVideo(videoId)
+
+        return binding.root
+    }
+
+    @SuppressLint("SetJavaScriptEnabled")
+    private fun setupWebView() {
+        binding.webView.apply {
+            settings.javaScriptEnabled = true
+            settings.mediaPlaybackRequiresUserGesture = false
+            settings.allowContentAccess = true
+            webViewClient = createWebViewClient()
+        }
+    }
+
+    private fun createWebViewClient() = object : WebViewClient() {
+        override fun shouldOverrideUrlLoading(view: WebView, request: WebResourceRequest): Boolean {
+            return true // Farklı bir URL açılmak istendiğinde engelle
+        }
+    }
+
+    private fun loadYouTubeVideo(videoId: String) {
+        val iframeHtml = generateIframeHtml(videoId)
+        CoroutineScope(Dispatchers.Main).launch {
+            binding.webView.loadDataWithBaseURL("https://www.youtube.com", iframeHtml, "text/html", "utf-8", null)
+        }
+    }
+
+    private fun generateIframeHtml(videoId: String): String {
+        return """
+            <html>
+            <body style="margin:0;padding:0;">
+                <iframe 
+                    id="player" 
+                    type="text/html" 
+                    width="100%"
+                    height="100%" 
+                    src="https://www.youtube.com/embed/$videoId?enablejsapi=1&controls=0&modestbranding=1&rel=0&autohide=1&autoplay=0" 
+                    frameborder="0"
+                    allowfullscreen>
+                </iframe>
+                <script>
+                    var tag = document.createElement('script');
+                    tag.src = "https://www.youtube.com/iframe_api";
+                    var firstScriptTag = document.getElementsByTagName('script')[0];
+                    firstScriptTag.parentNode.insertBefore(tag, firstScriptTag);
+                </script>
+            </body>
+            </html>
+        """.trimIndent()
+    }
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        binding.webView.destroy() // WebView'i yok et
+        _binding = null
+    }
+}
