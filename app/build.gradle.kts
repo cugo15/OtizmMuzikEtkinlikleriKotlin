49f5fcc6d63b12e8c8af45ff38b124d4be0f72ee
Cumhur Günay <cugo2435@gmail.com>
2024-11-04 12:44:44 +0300

    activity ktx version update

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -50,7 +50,7 @@ dependencies {
     implementation("com.google.android.material:material:1.12.0")
     implementation("androidx.constraintlayout:constraintlayout:2.1.4")
     implementation("androidx.core:core-ktx:1.13.1")
-    implementation("androidx.activity:activity:1.9.2")
+    implementation("androidx.activity:activity-ktx:1.9.3")
     testImplementation("junit:junit:4.13.2")
     androidTestImplementation("androidx.test.ext:junit:1.2.1")
     androidTestImplementation("androidx.test.espresso:espresso-core:3.6.1")
@@ -75,5 +75,9 @@ dependencies {
 
     //Görsel Yükleme: Glide kütüphanesi
     implementation("com.github.bumptech.glide:glide:4.16.0")
+
+    implementation ("org.jetbrains.kotlinx:kotlinx-coroutines-core:1.5.2")
+    implementation ("org.jetbrains.kotlinx:kotlinx-coroutines-android:1.7.1")
+
 
 }
