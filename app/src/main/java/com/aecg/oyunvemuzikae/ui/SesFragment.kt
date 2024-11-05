7637792c0c16b2d8a1ccf6f273cad986c07a05ce
Cumhur Günay <cugo2435@gmail.com>
2024-11-05 14:35:51 +0300

    Useless imports Deleted

--- app/src/main/java/com/aecg/oyunvemuzikae/ui/SesFragment.kt
+++ app/src/main/java/com/aecg/oyunvemuzikae/ui/SesFragment.kt
@@ -2,9 +2,7 @@ package com.aecg.oyunvemuzikae.ui
 
 import android.media.MediaPlayer
 import android.os.Bundle
-import android.view.LayoutInflater
 import android.view.View
-import android.view.ViewGroup
 import android.view.animation.Animation
 import android.view.animation.AnimationUtils
 import androidx.recyclerview.widget.LinearLayoutManager
