c70d0375003e7916c3f5f563ab28d0b88e390a65
Cumhur Günay <cugo2435@gmail.com>
2024-11-07 17:16:20 +0300

    Added function to handle navigation

--- app/src/main/java/com/aecg/oyunvemuzikae/ui/HomeFragment.kt
+++ app/src/main/java/com/aecg/oyunvemuzikae/ui/HomeFragment.kt
@@ -2,10 +2,10 @@ package com.aecg.oyunvemuzikae.ui
 
 import android.os.Bundle
 import android.view.View
+import androidx.navigation.NavDirections
 import androidx.navigation.fragment.findNavController
 import com.aecg.oyunvemuzikae.BaseFragment
 import com.aecg.oyunvemuzikae.MyApplication
-import com.aecg.oyunvemuzikae.R
 import com.aecg.oyunvemuzikae.databinding.FragmentHomeBinding
 
 class HomeFragment : BaseFragment<FragmentHomeBinding>(FragmentHomeBinding::inflate) {
@@ -13,21 +13,22 @@ class HomeFragment : BaseFragment<FragmentHomeBinding>(FragmentHomeBinding::infl
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
         val myApplication = requireActivity().application as MyApplication
+
         binding.btnHomePiyano.setOnClickListener {
-            findNavController().navigate(R.id.homeFragment_to_pianoFragment)
+            navigateToFragment(HomeFragmentDirections.homeFragmentToPianoFragment())
         }
         binding.btnHomeSesler.setOnClickListener {
-            val action = HomeFragmentDirections.homeFragmentToMenuFragment(myApplication.sesMenuList.toTypedArray())
-            findNavController().navigate(action)
+            navigateToFragment(HomeFragmentDirections.homeFragmentToMenuFragment(myApplication.sesMenuList.toTypedArray()))
         }
         binding.btnHomeOyunlar.setOnClickListener {
-            val action = HomeFragmentDirections.homeFragmentToMenuFragment(myApplication.oyunMenuList.toTypedArray())
-            findNavController().navigate(action)
+            navigateToFragment(HomeFragmentDirections.homeFragmentToMenuFragment(myApplication.oyunMenuList.toTypedArray()))
         }
         binding.btnHomeMuzikler.setOnClickListener {
-            val action = HomeFragmentDirections.homeFragmentToMuzikFragment(myApplication.muzikMenuList.toTypedArray())
-            findNavController().navigate(action)
+            navigateToFragment(HomeFragmentDirections.homeFragmentToMuzikFragment(myApplication.muzikMenuList.toTypedArray()))
         }
+    }
+    private fun navigateToFragment(action: NavDirections) {
+        findNavController().navigate(action)
     }
 
 }
