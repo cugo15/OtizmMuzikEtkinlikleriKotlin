ba07418ddc8d31e35f788aed2d56bab05b539ee1
Cumhur Günay <cugo2435@gmail.com>
2024-11-08 16:39:46 +0300

    Enum class converted to Sealed class for using nested classes

--- app/src/main/java/com/aecg/oyunvemuzikae/Sesler/SesModel.kt
+++ app/src/main/java/com/aecg/oyunvemuzikae/Sesler/SesModel.kt
@@ -2,11 +2,12 @@ package com.aecg.oyunvemuzikae.Sesler
 
 import android.os.Parcelable
 import kotlinx.parcelize.Parcelize
+import kotlinx.parcelize.RawValue
 
 @Parcelize
 data class SesModel(
     val sesName: String,
-    val type: SesType, // type enum olarak tanımlı
+    val type: @RawValue SesType, // type enum olarak tanımlı
     val sesResourceId: Int, // raw klasöründeki ses dosyasını temsil eden ID
     val imageResourceId: Int
 ) : Parcelable
