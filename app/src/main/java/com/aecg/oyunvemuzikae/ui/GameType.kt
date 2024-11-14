2ec0c1ea0c78bb1a092df401a04cdee50d3295c7
Cumhur Günay <cugo2435@gmail.com>
2024-11-14 15:26:06 +0300

    created gametype enum class

---
+++ app/src/main/java/com/aecg/oyunvemuzikae/ui/GameType.kt
@@ -0,0 +1,9 @@
+package com.aecg.oyunvemuzikae.ui
+
+enum class GameType(val displayName: String) {
+    HIZLIYAVAS("Hızlı Yavaş"),
+    RESIMDENBUL("Resimden Bul"),
+    SESTENBUL("Sesten Bul"),
+    HAFIZA("Hafıza"),
+    ENSTRUMANTIPI("Enstrüman Tipi"),
+}
