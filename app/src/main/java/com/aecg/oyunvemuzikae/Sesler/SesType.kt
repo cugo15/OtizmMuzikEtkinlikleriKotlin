6a756e6a9894b083f5abf3253c91dbafeec7359a
Cumhur Günay <cugo2435@gmail.com>
2024-11-14 15:27:20 +0300

    gameHeader parameter added

--- app/src/main/java/com/aecg/oyunvemuzikae/Sesler/SesType.kt
+++ app/src/main/java/com/aecg/oyunvemuzikae/Sesler/SesType.kt
@@ -1,22 +1,25 @@
 package com.aecg.oyunvemuzikae.Sesler
 
-// Ana sealed class olan SesType
 sealed class SesType(val displayName: String) {
 
-    object HAYVAN : SesType("Hayvan Sesleri")
-    object INSAN : SesType("İnsan Sesleri")
-    object DOGA : SesType("Doğa Sesleri")
-    object ARAC : SesType("Araç Sesleri")
-    object SAYI : SesType("Sayılar")
-    object SEKIL : SesType("Geometrik Şekiller")
+    data object HAYVAN : SesType("Hayvan Sesleri")
+    data object INSAN : SesType("İnsan Sesleri")
+    data object DOGA : SesType("Doğa Sesleri")
+    data object ARAC : SesType("Araç Sesleri")
+    data object SAYI : SesType("Sayılar")
+    data object SEKIL : SesType("Geometrik Şekiller")
 
-    // ENSTRUMAN türü için sealed class ve alt enum class tanımlıyoruz
-    sealed class ENSTRUMAN(displayName: String) : SesType(displayName) {
-        object ORFF : ENSTRUMAN("Enstrüman Sesleri")
-        object VURMALI : ENSTRUMAN("Enstrüman Sesleri")
-        object TELLI : ENSTRUMAN("Enstrüman Sesleri")
-        object UFLEMELI : ENSTRUMAN("Enstrüman Sesleri")
-        object OTHER : ENSTRUMAN("Enstrüman Sesleri")
+    // ENSTRUMAN türü için sealed class ve alt sınıfları tanımlıyoruz
+    sealed class ENSTRUMAN(
+        displayName: String = "Enstrüman Sesleri",
+        val displayGameHeader: String
+    ) : SesType(displayName) {
+        data object ORFF : ENSTRUMAN(displayGameHeader = "Orff")
+        data object VURMALI : ENSTRUMAN(displayGameHeader = "Vurmalı")
+        data object TELLI : ENSTRUMAN(displayGameHeader = "Telli")
+        data object UFLEMELI : ENSTRUMAN(displayGameHeader = "Üflemeli")
+        data object OTHER : ENSTRUMAN(displayGameHeader = "Diğer")
     }
 }
+
 
