f47733e8b0ef4ce41342da111a24e79df2bda2ed
Cumhur Günay <cugo2435@gmail.com>
2024-11-21 14:14:54 +0300

    Bind piano repository

--- app/src/main/java/com/aecg/oyunvemuzikae/app/RepositoryModule.kt
+++ app/src/main/java/com/aecg/oyunvemuzikae/app/RepositoryModule.kt
@@ -2,6 +2,8 @@ package com.aecg.oyunvemuzikae.app
 
 import com.aecg.oyunvemuzikae.data.repository.MenuRepository
 import com.aecg.oyunvemuzikae.data.repository.MenuRepositoryImpl
+import com.aecg.oyunvemuzikae.data.repository.PianoRepository
+import com.aecg.oyunvemuzikae.data.repository.PianoRepositoryImpl
 import dagger.Binds
 import dagger.Module
 import dagger.hilt.InstallIn
@@ -12,4 +14,6 @@ import dagger.hilt.components.SingletonComponent
 abstract class RepositoryModule {
     @Binds
     abstract fun bindMenuRepository(menuRepositoryImpl: MenuRepositoryImpl): MenuRepository
+    @Binds
+    abstract fun bindPianoRepository(pianoRepositoryImpl: PianoRepositoryImpl): PianoRepository
 }
