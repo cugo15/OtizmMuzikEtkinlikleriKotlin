2daf51527b46d31cb6569fc3bc0fe99f2f344f8b
Cumhur Günay <cugo2435@gmail.com>
2024-11-21 14:12:44 +0300

    Created new module class for providing context and soundpool

---
+++ app/src/main/java/com/aecg/oyunvemuzikae/app/AppModule.kt
@@ -0,0 +1,27 @@
+package com.aecg.oyunvemuzikae.app
+
+import android.app.Application
+import android.content.Context
+import android.media.SoundPool
+import dagger.Module
+import dagger.Provides
+import dagger.hilt.InstallIn
+import dagger.hilt.components.SingletonComponent
+import javax.inject.Singleton
+
+@Module
+@InstallIn(SingletonComponent::class)
+object AppModule {
+
+    @Provides
+    @Singleton
+    fun provideContext(app: Application): Context {
+        return app.applicationContext
+    }
+    @Provides
+    fun provideSoundPool(): SoundPool {
+        return SoundPool.Builder()
+            .setMaxStreams(10)
+            .build()
+    }
+}
