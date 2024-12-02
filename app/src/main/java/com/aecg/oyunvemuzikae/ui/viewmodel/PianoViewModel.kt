6b86ba4694b9c154bf7b375d7f0c5df2801f3eee
Cumhur Günay <cugo2435@gmail.com>
2024-12-02 14:34:11 +0300

    Functions made single expression and new functions added

--- app/src/main/java/com/aecg/oyunvemuzikae/ui/viewmodel/PianoViewModel.kt
+++ app/src/main/java/com/aecg/oyunvemuzikae/ui/viewmodel/PianoViewModel.kt
@@ -1,6 +1,5 @@
 package com.aecg.oyunvemuzikae.ui.viewmodel
 
-import android.widget.Button
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
 import com.aecg.oyunvemuzikae.data.repository.PianoRepository
@@ -10,20 +9,12 @@ import javax.inject.Inject
 
 @HiltViewModel
 class PianoViewModel @Inject constructor(private val pianoRepository: PianoRepository) : ViewModel() {
-    private val doNotation = pianoRepository.getDoNotation()
-    private val pitchNotation = pianoRepository.getPitchNotation()
-    private val clearNotation = pianoRepository.getClearNotation()
 
-    private val pianoColors = pianoRepository.getPianoKeyboardColors()
-
-    fun getClearNotation() = clearNotation
-    fun getDoNotation() = doNotation
-    fun getPitchNotation() = pitchNotation
-    fun playSound(button: Button) = pianoRepository.playSound(button)
-    fun getColorForKey(index: Int): Int {
-        return pianoColors[index % pianoColors.size]
-    }
-
+    fun getNotationAction(buttonText: String, isFixedDo: Boolean) = pianoRepository.getNotationAction(buttonText, isFixedDo)
+    fun playSound(tag: String) = pianoRepository.playSound(tag)
+    fun getColorForKey(index: Int):Int = pianoRepository.getColorForKey(index)
+    fun getColorForTxt(index: Int):Int = pianoRepository.getColorForTxt(index)
+    fun calculateButtonWidth(increase: Boolean, currentWidth: Int, increment: Int): Int = pianoRepository.calculateButtonWidth(increase, currentWidth, increment)
     fun loadSounds() {
         viewModelScope.launch {
             pianoRepository.loadSounds()
