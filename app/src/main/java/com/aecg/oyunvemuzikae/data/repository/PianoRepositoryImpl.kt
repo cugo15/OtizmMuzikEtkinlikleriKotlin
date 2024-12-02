18ac6dd0d6b86027aef97bb886f7e189fdbee2b0
Cumhur Günay <cugo2435@gmail.com>
2024-12-02 14:35:12 +0300

    impl new functions

--- app/src/main/java/com/aecg/oyunvemuzikae/data/repository/PianoRepositoryImpl.kt
+++ app/src/main/java/com/aecg/oyunvemuzikae/data/repository/PianoRepositoryImpl.kt
@@ -3,43 +3,38 @@ package com.aecg.oyunvemuzikae.data.repository
 import android.content.Context
 import android.media.SoundPool
 import android.util.Log
-import android.widget.Button
 import com.aecg.oyunvemuzikae.data.datasource.PianoDataSource
-import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.async
-import kotlinx.coroutines.launch
 import kotlinx.coroutines.withContext
 import javax.inject.Inject
 
 class PianoRepositoryImpl @Inject constructor(
-    private val pianoDataSource: PianoDataSource,
+    pianoDataSource: PianoDataSource,
     private val context: Context,
     private val soundPool: SoundPool
 ) : PianoRepository {
-    override fun getWhitePianoSounds() = pianoDataSource.getWhitePianoSounds()
-    override fun getBlackPianoSounds() = pianoDataSource.getBlackPianoSounds()
-    override fun getDoNotation() = pianoDataSource.getDoNotation()
-    override fun getPitchNotation() = pianoDataSource.getPitchNotation()
-    override fun getClearNotation() = pianoDataSource.getClearNotation()
 
-    override fun getPianoKeyboardColors(): Array<Int> = pianoDataSource.getPianoKeyboardColors()
-
+    override val whitePianoSounds: Array<Int> = pianoDataSource.getWhitePianoSounds()
+    override val blackPianoSounds: Array<Int> = pianoDataSource.getBlackPianoSounds()
+    override val doNotation: Array<String> = pianoDataSource.getDoNotation()
+    override val pitchNotation: Array<String> = pianoDataSource.getPitchNotation()
+    override val clearNotation: Array<String> = pianoDataSource.getClearNotation()
+    override val pianoKeyboardColors: Array<Int> = pianoDataSource.getPianoKeyboardColors()
+    override val pianoKeyboardTxtColors: Array<Int> = pianoDataSource.getPianoKeyboardTxt()
     private val soundMap = mutableMapOf<String, Int>()
-    // Seslerin yüklenmesi
+
     override suspend fun loadSounds() {
         // IO thread üzerinde işlemleri başlatıyoruz
         withContext(Dispatchers.IO) {
             val whiteSounds = async {
-                getWhitePianoSounds().forEachIndexed { index, soundRes ->
-                    // Yükleme işlemi hemen başlayacak, ancak sesin yüklenmesini bekleyeceğiz
+                whitePianoSounds.forEachIndexed { index, soundRes ->
                     val soundId = soundPool.load(context, soundRes, 1)
                     soundMap["white_$index"] = soundId
                 }
             }
             val blackSounds = async {
-                getBlackPianoSounds().forEachIndexed { index, soundRes ->
-                    // Aynı şekilde, her bir sesin yüklenmesini bekliyoruz
+                blackPianoSounds.forEachIndexed { index, soundRes ->
                     val soundId = soundPool.load(context, soundRes, 1)
                     soundMap["black_$index"] = soundId
                 }
@@ -52,14 +47,51 @@ class PianoRepositoryImpl @Inject constructor(
         }
     }
 
+    override fun getColorForKey(index: Int): Int = pianoKeyboardColors[index % pianoKeyboardColors.size]
 
-    // Butonla eşleşen sesi çalma
-    override fun playSound(button: Button) {
-            val soundId = button.tag?.toString()?.let { soundMap[it] }
+    override fun getColorForTxt(index: Int): Int {
+        if (index in 0..6){
+            return pianoKeyboardTxtColors[0]
+        }
+        else if (index in 7..13){
+            return pianoKeyboardTxtColors[1]
+        }
+        else if (index in 14..20){
+            return pianoKeyboardTxtColors[2]
+        }
+        else if (index in 21..27){
+            return pianoKeyboardTxtColors[3]
+        }
+        else if (index in 28..34){
+            return pianoKeyboardTxtColors[4]
+        }
+        else{
+            return pianoKeyboardTxtColors[5]
+        }
+    }
+
+
+
+    override fun playSound(tag: String) {
+            val soundId = soundMap[tag]
             soundId?.let {
                 soundPool.play(it, 1f, 1f, 0, 0, 1f)
             }
+    }
 
+    override fun getNotationAction(buttonText: String, isFixedDo: Boolean): Array<String> {
+        return when (buttonText) {
+            "Do2" -> if (isFixedDo) clearNotation else pitchNotation
+            "C2" -> if (isFixedDo) doNotation else clearNotation
+            else -> if (isFixedDo) doNotation else pitchNotation
+        }
+    }
+    override fun calculateButtonWidth(increase: Boolean, currentWidth: Int, increment: Int): Int {
+        return when {
+            increase -> currentWidth + increment
+            !increase -> currentWidth - increment
+            else -> currentWidth
+        }
     }
     override fun release() {
         soundPool.release()
