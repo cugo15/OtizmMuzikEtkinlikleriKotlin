24af1457ea2f036caab692dab4fb319116e8ac95
Cumhur Günay <cugo2435@gmail.com>
2024-12-10 16:24:06 +0300

    impl MVVM architecture

--- app/src/main/java/com/aecg/oyunvemuzikae/data/repository/MenuRepository.kt
+++ app/src/main/java/com/aecg/oyunvemuzikae/data/repository/MenuRepository.kt
@@ -2,9 +2,21 @@ package com.aecg.oyunvemuzikae.data.repository
 
 import com.aecg.oyunvemuzikae.data.model.MenuModel
 import com.aecg.oyunvemuzikae.data.model.MuzikModel
+import com.aecg.oyunvemuzikae.data.model.SesModel
 
 interface MenuRepository {
-    fun getSesMenuList(): Array<MenuModel>
-    fun getOyunMenuList(): Array<MenuModel>
-    fun getMuzikMenuList(): Array<MuzikModel>
+    val sesMenuList: Array<MenuModel>
+    val oyunMenuList: Array<MenuModel>
+    val muzikMenuList: Array<MuzikModel>
+    val enstrumanList: Array<SesModel>
+    val dogaList: Array<SesModel>
+    val insanList: Array<SesModel>
+    val aracList: Array<SesModel>
+    val hayvanList: Array<SesModel>
+    val sekilList: Array<SesModel>
+    val sayiList: Array<SesModel>
+    val oyunHizliYavasList: Array<SesModel>
+    val oyunResimdenBulList: Array<SesModel>
+    val oyunMemoryListMenu: Array<MenuModel>
+
 }
--- app/src/main/java/com/aecg/oyunvemuzikae/data/repository/MenuRepositoryImpl.kt
+++ app/src/main/java/com/aecg/oyunvemuzikae/data/repository/MenuRepositoryImpl.kt
@@ -2,15 +2,30 @@ package com.aecg.oyunvemuzikae.data.repository
 
 import com.aecg.oyunvemuzikae.data.datasource.MenuDataSource
 import com.aecg.oyunvemuzikae.data.datasource.MuzikDataSource
+import com.aecg.oyunvemuzikae.data.datasource.SesDataSource
 import com.aecg.oyunvemuzikae.data.model.MenuModel
 import com.aecg.oyunvemuzikae.data.model.MuzikModel
+import com.aecg.oyunvemuzikae.data.model.SesModel
 import javax.inject.Inject
 
 class MenuRepositoryImpl @Inject constructor(
-    private val menuDataSource: MenuDataSource,
-    private val muzikDataSource: MuzikDataSource
+    menuDataSource: MenuDataSource,
+    muzikDataSource: MuzikDataSource,
+    sesDataSource: SesDataSource
 ) :MenuRepository{
-    override fun getSesMenuList(): Array<MenuModel> = menuDataSource.getSesMenuList()
-    override fun getOyunMenuList(): Array<MenuModel> = menuDataSource.getOyunMenuList()
-    override fun getMuzikMenuList(): Array<MuzikModel> = muzikDataSource.getMuzikList()
+    override val sesMenuList: Array<MenuModel> = menuDataSource.getSesMenuList()
+    override val oyunMenuList: Array<MenuModel> = menuDataSource.getOyunMenuList()
+    override val oyunMemoryListMenu: Array<MenuModel> = menuDataSource.getOyunMemoryListMenu()
+
+    override val muzikMenuList: Array<MuzikModel> = muzikDataSource.getMuzikList()
+
+    override val enstrumanList: Array<SesModel> = sesDataSource.getEnstrumanList()
+    override val dogaList: Array<SesModel> = sesDataSource.getDogaList()
+    override val insanList: Array<SesModel> = sesDataSource.getInsanList()
+    override val aracList: Array<SesModel> = sesDataSource.getAracList()
+    override val hayvanList: Array<SesModel> = sesDataSource.getHayvanList()
+    override val sekilList: Array<SesModel> = sesDataSource.getSekilList()
+    override val sayiList: Array<SesModel> = sesDataSource.getSayiList()
+    override val oyunHizliYavasList: Array<SesModel> = sesDataSource.getOyunHizliYavasList()
+    override val oyunResimdenBulList: Array<SesModel> = sesDataSource.getOyunResimdenBulList()
 }
