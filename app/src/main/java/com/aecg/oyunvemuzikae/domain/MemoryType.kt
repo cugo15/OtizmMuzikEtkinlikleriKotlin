ca9fc0a9c6a9a9afa6cde2645bc1281206ebef10
Cumhur Günay <cugo2435@gmail.com>
2024-12-10 16:22:34 +0300

    Created MemoryTypeEnumClass

---
+++ app/src/main/java/com/aecg/oyunvemuzikae/domain/MemoryType.kt
@@ -0,0 +1,9 @@
+package com.aecg.oyunvemuzikae.domain
+
+enum class MemoryType (val displayName: String,val difficulty: Int,val level: Int) {
+    LVL1("Seviye 1",1,1),
+    LVL2("Seviye 2",2,6),
+    LVL3("Seviye 3",3,11),
+    LVL4("Seviye 4",4,16),
+    LVL5("Seviye 5",5,21),
+}
