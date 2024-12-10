a2d203dd170ae670ff78500d1f8f9df303b41cbf
Cumhur Günay <cugo2435@gmail.com>
2024-12-10 16:23:29 +0300

    business logics moved to viewmodel

--- app/src/main/java/com/aecg/oyunvemuzikae/ui/fragment/MenuFragment.kt
+++ app/src/main/java/com/aecg/oyunvemuzikae/ui/fragment/MenuFragment.kt
@@ -8,11 +8,9 @@ import androidx.fragment.app.viewModels
 import androidx.navigation.fragment.findNavController
 import androidx.recyclerview.widget.LinearLayoutManager
 import com.aecg.oyunvemuzikae.domain.MenuType
-import com.aecg.oyunvemuzikae.app.MyApplication
 import com.aecg.oyunvemuzikae.R
 import com.aecg.oyunvemuzikae.base.BaseFragment
 import com.aecg.oyunvemuzikae.databinding.FragmentMenuBinding
-import com.aecg.oyunvemuzikae.domain.GameType
 import com.aecg.oyunvemuzikae.ui.adapter.MenuAdapter
 import com.aecg.oyunvemuzikae.data.model.MenuModel
 import com.aecg.oyunvemuzikae.ui.viewmodel.MenuViewModel
@@ -26,89 +24,35 @@ class MenuFragment : BaseFragment<FragmentMenuBinding>(FragmentMenuBinding::infl
     private lateinit var animationZoom: Animation
     private lateinit var menuList: ArrayList<MenuModel>
 
-    private val myApplication: MyApplication by lazy {
-        requireActivity().application as MyApplication
-    }
-
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
         animationZoom = AnimationUtils.loadAnimation(requireContext(), R.anim.zoom_inshort)
-
         menuList = MenuFragmentArgs.fromBundle(requireArguments()).menuList.toList() as ArrayList<MenuModel>
-        val category = menuList[1].type
+        val category = menuList[0].type
+        setupLayout(category)
         setupRecyclerView(menuList)
-        if (category== MenuType.MEMORYLEVEL){
-         binding.rvMenu.background = null
-        }
-        binding.textViewMenuHeader.text = category.displayName
-
-        val backgroundResourceMap = mapOf(
-            MenuType.SES to R.drawable.bg_menu_ses,
-            MenuType.OYUN to R.drawable.bg_menu_oyun,
-            MenuType.MEMORYLEVEL to R.drawable.bg_oyun_menu_hafiza,
-        )
-        val backgroundResource = backgroundResourceMap[category] ?: R.drawable.bg_doga
-
-        binding.layoutMenu.loadLayoutBackgroundWithGlide(requireContext(), backgroundResource)
-
-        binding.btnScrollLeftMenu.setOnClickListener {
-            binding.rvMenu.scrollInDirection(-1)
-        }
-
-        binding.BtnScrollRightMenu.setOnClickListener {
-            binding.rvMenu.scrollInDirection(1)
+        setupScrollButtons()
+    }
+    private fun setupRecyclerView(menuList: ArrayList<MenuModel>) {
+        binding.rvMenu.layoutManager = LinearLayoutManager(requireContext(), LinearLayoutManager.HORIZONTAL, false)
+        binding.rvMenu.adapter = MenuAdapter(menuList, { name, type ->
+            menuViewModel.handleMenuClick(name, type)?.let { action ->
+                findNavController().navigate(action)
+            }
+        }) { view ->
+            view.startAnimation(animationZoom)
         }
     }
 
-    private fun setupRecyclerView(menuList: ArrayList<MenuModel>) {
-        // RecyclerView'e yatay (horizontal) LinearLayoutManager ata
-        binding.rvMenu.layoutManager = LinearLayoutManager(requireContext(), LinearLayoutManager.HORIZONTAL, false)
-        // SesAdapter'ı RecyclerView'e ata
-        binding.rvMenu.adapter = MenuAdapter(menuList, { name,type ->
-            if (type == MenuType.SES) {
-                val action = when (name) {
-                    "Enstrüman" -> MenuFragmentDirections.menuFragmentToSesFragment(myApplication.enstrumanList.toTypedArray())
-                    "Doğa" -> MenuFragmentDirections.menuFragmentToSesFragment(myApplication.dogaList.toTypedArray())
-                    "İnsan" -> MenuFragmentDirections.menuFragmentToSesFragment(myApplication.insanList.toTypedArray())
-                    "Araçlar" -> MenuFragmentDirections.menuFragmentToSesFragment(myApplication.aracList.toTypedArray())
-                    "Hayvan" -> MenuFragmentDirections.menuFragmentToSesFragment(myApplication.hayvanList.toTypedArray())
-                    "Şekiller" -> MenuFragmentDirections.menuFragmentToSesFragment(myApplication.sekilList.toTypedArray())
-                    "Sayılar" -> MenuFragmentDirections.menuFragmentToSesFragment(myApplication.sayiList.toTypedArray())
-                    else -> null // Geçersiz bir isim durumunda
-                }
-                action?.let { findNavController().navigate(it) }
-            }else if(type == MenuType.OYUN){
-                val action = when (name) {
-                    GameType.HIZLIYAVAS.displayName -> MenuFragmentDirections.menuFragmentToOyunHizliYavasFragment(myApplication.oyunHizliYavasList.toTypedArray())
-                    GameType.RESIMDENBUL.displayName -> MenuFragmentDirections.menuFragmentToOyunResimdenSestenBulFragment(myApplication.oyunResimdenBulList.toTypedArray(),
-                        GameType.RESIMDENBUL
-                    )
-                    GameType.SESTENBUL.displayName -> MenuFragmentDirections.menuFragmentToOyunResimdenSestenBulFragment(myApplication.enstrumanList.toTypedArray(),
-                        GameType.SESTENBUL
-                    )
-                    GameType.ENSTRUMANTIPI.displayName -> MenuFragmentDirections.menuFragmentToOyunResimdenSestenBulFragment(myApplication.enstrumanList.toTypedArray(),
-                        GameType.ENSTRUMANTIPI
-                    )
-                    GameType.HAFIZA.displayName -> MenuFragmentDirections.menuFragmentToSelf(myApplication.oyunMemoryMenuList.toTypedArray())
-                    else -> null // Geçersiz bir isim durumunda
-                }
-                action?.let { findNavController().navigate(it) }
-            }else if(type == MenuType.MEMORYLEVEL){
-                val action = when (name) {
-                    "Seviye 1" -> MenuFragmentDirections.menuFragmentToOyunHafizaFragment(myApplication.enstrumanList.toTypedArray(),1,1)
-                    "Seviye 2" -> MenuFragmentDirections.menuFragmentToOyunHafizaFragment(myApplication.enstrumanList.toTypedArray(),2,6)
-                    "Seviye 3" -> MenuFragmentDirections.menuFragmentToOyunHafizaFragment(myApplication.enstrumanList.toTypedArray(),3,11)
-                    "Seviye 4" -> MenuFragmentDirections.menuFragmentToOyunHafizaFragment(myApplication.enstrumanList.toTypedArray(),4,16)
-                    "Seviye 5" -> MenuFragmentDirections.menuFragmentToOyunHafizaFragment(myApplication.enstrumanList.toTypedArray(),5,21)
-                    else -> null // Geçersiz bir isim durumunda
-                }
-                action?.let { findNavController().navigate(it) }
-            }
-
-
-        }) { view ->
-            // Animasyonu başlat
-            view.startAnimation(animationZoom)
+    private fun setupLayout(category: MenuType){
+        if (category== MenuType.MEMORYLEVEL){ binding.rvMenu.background = null }
+        binding.textViewMenuHeader.text = category.displayName
+        binding.layoutMenu.loadLayoutBackgroundWithGlide(requireContext(), category.backgroundResource)
+    }
+    private fun setupScrollButtons(){
+        with(binding){
+            btnScrollLeftMenu.setOnClickListener{rvMenu.scrollInDirection(-1)}
+            BtnScrollRightMenu.setOnClickListener {rvMenu.scrollInDirection(1)}
         }
     }
 }
