dc12b6b1d7ce33cc36fffb0cd9910be26adbfbff
Cumhur Günay <cugo2435@gmail.com>
2024-12-18 15:32:18 +0300

    muziklist removed

--- app/src/main/java/com/aecg/oyunvemuzikae/ui/viewmodel/HomeViewModel.kt
+++ app/src/main/java/com/aecg/oyunvemuzikae/ui/viewmodel/HomeViewModel.kt
@@ -3,7 +3,6 @@ package com.aecg.oyunvemuzikae.ui.viewmodel
 
 import androidx.lifecycle.ViewModel
 import com.aecg.oyunvemuzikae.data.model.MenuModel
-import com.aecg.oyunvemuzikae.data.model.MuzikModel
 import com.aecg.oyunvemuzikae.data.repository.MenuRepository
 import dagger.hilt.android.lifecycle.HiltViewModel
 import javax.inject.Inject
@@ -14,11 +13,9 @@ class HomeViewModel @Inject constructor(menuRepository: MenuRepository) : ViewMo
     // Menü listelerini doğrudan saklıyoruz
     private val sesMenuList: Array<MenuModel> = menuRepository.sesMenuList
     private val oyunMenuList: Array<MenuModel> = menuRepository.oyunMenuList
-    private val muzikMenuList: Array<MuzikModel> = menuRepository.muzikMenuList
 
     // Getter metodları ile verilere erişim sağlıyoruz
     fun getSesMenuList(): Array<MenuModel> = sesMenuList
     fun getOyunMenuList(): Array<MenuModel> = oyunMenuList
-    fun getMuzikMenuList(): Array<MuzikModel> = muzikMenuList
 }
 
