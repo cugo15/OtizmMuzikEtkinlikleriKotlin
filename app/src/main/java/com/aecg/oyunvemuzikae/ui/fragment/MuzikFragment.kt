ae79570aca0be4856066589190280022766b4d80
Cumhur Günay <cugo2435@gmail.com>
2024-12-18 15:34:23 +0300

    Only UI Logics handled in fragment

--- app/src/main/java/com/aecg/oyunvemuzikae/ui/fragment/MuzikFragment.kt
+++ app/src/main/java/com/aecg/oyunvemuzikae/ui/fragment/MuzikFragment.kt
@@ -1,74 +1,49 @@
 package com.aecg.oyunvemuzikae.ui.fragment
 
-import android.media.MediaPlayer
-import android.net.Uri
 import android.os.Bundle
 import android.view.View
 import android.view.animation.Animation
 import android.view.animation.AnimationUtils
+import androidx.fragment.app.viewModels
 import androidx.navigation.fragment.findNavController
 import androidx.recyclerview.widget.LinearLayoutManager
 import com.aecg.oyunvemuzikae.R
-import com.aecg.oyunvemuzikae.base.BaseFragment
+import com.aecg.oyunvemuzikae.ui.fragment.base.BaseFragment
 import com.aecg.oyunvemuzikae.databinding.FragmentMuzikBinding
 import com.aecg.oyunvemuzikae.ui.adapter.MuzikAdapter
 import com.aecg.oyunvemuzikae.data.model.MuzikModel
-import com.aecg.oyunvemuzikae.domain.MuzikType
+import com.aecg.oyunvemuzikae.ui.viewmodel.MuzikViewModel
+import dagger.hilt.android.AndroidEntryPoint
 
+@AndroidEntryPoint
 class MuzikFragment : BaseFragment<FragmentMuzikBinding>(FragmentMuzikBinding::inflate) {
 
-    private lateinit var mediaPlayer: MediaPlayer
-    private lateinit var muzikList: ArrayList<MuzikModel>
-    private lateinit var animationzoom: Animation
+    private val muzikViewModel: MuzikViewModel by viewModels()
+    private lateinit var animationZoom: Animation
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
-        animationzoom = AnimationUtils.loadAnimation(requireContext(), R.anim.zoom_inshort)
-        muzikList = MuzikFragmentArgs.fromBundle(requireArguments()).muzikList.toCollection(ArrayList())
+        animationZoom = AnimationUtils.loadAnimation(requireContext(), R.anim.zoom_inshort)
+        val muzikList = muzikViewModel.muzikList
         setupRecyclerView(muzikList)
     }
 
-    private fun setupRecyclerView(muzikList: ArrayList<MuzikModel>) {
-        // RecyclerView'e yatay (horizontal) LinearLayoutManager ata
-        binding.rvMuzikler.layoutManager = LinearLayoutManager(requireContext(), LinearLayoutManager.HORIZONTAL, false)
-        // SesAdapter'ı RecyclerView'e ata
-        binding.rvMuzikler.adapter = MuzikAdapter(muzikList, { uri,type ->
-            if (type== MuzikType.MUZIK){
-                playSoundByUri(uri)
-            }else{
-                openVideo(uri)
-            }
-        }) { view ->
-            // Animasyonu başlat
-            view.startAnimation(animationzoom)
-        }
-    }
-
-    private fun playSoundByUri(uri: String) {
-        // Önce mevcut MediaPlayer nesnesini serbest bırak
-        if (::mediaPlayer.isInitialized) {
-            mediaPlayer.release()
-        }
-        // Yeni MediaPlayer nesnesi oluştur ve URI ayarla
-        mediaPlayer = MediaPlayer().apply {
-            setDataSource(requireContext(), Uri.parse(uri))
-            prepare() // Asenkron yükleme gerekiyorsa prepareAsync() kullanabilirsiniz
-            start()
-        }
-        // Ses tamamlandığında MediaPlayer'ı serbest bırak
-        mediaPlayer.setOnCompletionListener {
-            it.release()
+    private fun setupRecyclerView(muzikList: Array<MuzikModel>) {
+        binding.rvMuzikler.apply {
+            layoutManager = LinearLayoutManager(requireContext(), LinearLayoutManager.HORIZONTAL, false)
+            adapter = MuzikAdapter(
+                muzikList = muzikList,
+                onItemClick = { uri, type ->
+                    muzikViewModel.onMusicItemClicked(uri, type, ::openVideo)
+                },
+                onItemAnimate = { view ->
+                    view.startAnimation(animationZoom)
+                }
+            )
         }
     }
     private fun openVideo(videoId: String) {
         val action = MuzikFragmentDirections.actionMuzikFragmentToWebViewDialogFragment(videoId)
         findNavController().navigate(action)
-    }
-
-    override fun onDestroyView() {
-        super.onDestroyView()
-        if (::mediaPlayer.isInitialized) {
-            mediaPlayer.release()
-        }
     }
 }
