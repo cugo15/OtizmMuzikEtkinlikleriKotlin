76a100274f2f555199926abb01520913bb828aac
Cumhur Günay <cugo2435@gmail.com>
2024-12-20 11:18:52 +0300

    MediaPlayer class made Lifecycle aware

--- app/src/main/java/com/aecg/oyunvemuzikae/core/mediaplayer/MediaPlayerManagerImpl.kt
+++ app/src/main/java/com/aecg/oyunvemuzikae/core/mediaplayer/MediaPlayerManagerImpl.kt
@@ -1,17 +1,39 @@
 package com.aecg.oyunvemuzikae.core.mediaplayer
 
 import android.media.MediaPlayer
+import androidx.lifecycle.DefaultLifecycleObserver
+import androidx.lifecycle.LifecycleOwner
+
 import javax.inject.Inject
 
 open class MediaPlayerManagerImpl @Inject constructor(protected val mediaPlayer: MediaPlayer) :
-    MediaPlayerManager {
+    MediaPlayerManager, DefaultLifecycleObserver {
+    private var isPaused = false
 
-    final override fun initialize(): MediaPlayer {
-        return mediaPlayer
+    final override fun initialize(): MediaPlayer = mediaPlayer
+    final override fun release() = mediaPlayer.release()
+    final override fun reset() = mediaPlayer.reset()
+
+    final override fun resumeIfPaused() {
+        if (isPaused) {
+            mediaPlayer.start()
+            isPaused = false
+        }
+    }
+    final override fun pauseIfPlaying() {
+        if (mediaPlayer.isPlaying) {
+            mediaPlayer.pause()
+            isPaused = true
+        }
     }
 
-    final override fun release() {
-        mediaPlayer.release()
-    }
+    override fun onStart(owner: LifecycleOwner) = resumeIfPaused()
+
+    override fun onResume(owner: LifecycleOwner) = resumeIfPaused()
+
+    override fun onPause(owner: LifecycleOwner) = pauseIfPlaying()
+
+    override fun onStop(owner: LifecycleOwner) = pauseIfPlaying()
+
 
 }
