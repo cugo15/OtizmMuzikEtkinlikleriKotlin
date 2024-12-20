c92605688a657bda2c79fa16f4d18ef1cf0df1b1
Cumhur Günay <cugo2435@gmail.com>
2024-12-20 11:22:16 +0300

    Sestype made Parcelable

--- app/src/main/java/com/aecg/oyunvemuzikae/data/model/SesModel.kt
+++ app/src/main/java/com/aecg/oyunvemuzikae/data/model/SesModel.kt
@@ -1,14 +1,13 @@
 package com.aecg.oyunvemuzikae.data.model
 
+import SesType
 import android.os.Parcelable
-import com.aecg.oyunvemuzikae.domain.SesType
 import kotlinx.parcelize.Parcelize
-import kotlinx.parcelize.RawValue
 
 @Parcelize
 data class SesModel(
     val sesName: String,
-    val type: @RawValue SesType, // type enum olarak tanımlı
+    val type: SesType, // type enum olarak tanımlı
     val sesResourceId: Int, // raw klasöründeki ses dosyasını temsil eden ID
     val imageResourceId: Int
 ) : Parcelable
