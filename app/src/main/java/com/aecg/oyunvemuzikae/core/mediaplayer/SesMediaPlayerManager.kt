6aeb807515d085acdbeb07e71c012fce5e389a41
Cumhur Günay <cugo2435@gmail.com>
2024-12-20 11:21:47 +0300

    child MediaPlayer class created

---
+++ app/src/main/java/com/aecg/oyunvemuzikae/core/mediaplayer/SesMediaPlayerManager.kt
@@ -0,0 +1,28 @@
+package com.aecg.oyunvemuzikae.core.mediaplayer
+
+import android.content.Context
+import android.media.MediaPlayer
+import android.net.Uri
+import java.io.IOException
+import javax.inject.Inject
+
+class SesMediaPlayerManager @Inject constructor(val context: Context, mediaPlayer: MediaPlayer
+) : MediaPlayerManagerImpl(mediaPlayer) {
+
+    override fun playSoundById(id: Int) {
+        try {
+            mediaPlayer.reset()
+            mediaPlayer.apply {
+                val uri = "android.resource://${context.packageName}/$id"
+                setDataSource(context, Uri.parse(uri))
+                prepare()
+                start()
+                setOnCompletionListener {
+                    it.reset()
+                }
+            }
+        } catch (e: IOException) {
+            e.printStackTrace()
+        }
+    }
+}
