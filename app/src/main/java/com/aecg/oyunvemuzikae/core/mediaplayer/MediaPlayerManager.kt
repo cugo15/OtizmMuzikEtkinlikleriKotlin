cdbd562a94733f01e6e60a2ce2de25fbeeec1302
Cumhur Günay <cugo2435@gmail.com>
2024-12-20 11:17:33 +0300

    reset(),resumeIfPaused(),pauseIfPlaying(),playSoundById(id:Int) added

--- app/src/main/java/com/aecg/oyunvemuzikae/core/mediaplayer/MediaPlayerManager.kt
+++ app/src/main/java/com/aecg/oyunvemuzikae/core/mediaplayer/MediaPlayerManager.kt
@@ -5,5 +5,10 @@ import android.media.MediaPlayer
 interface MediaPlayerManager {
     fun initialize(): MediaPlayer
     fun release()
+    fun reset()
+    fun resumeIfPaused()
+    fun pauseIfPlaying()
     fun playSoundByUri(uri: String){}
+    fun playSoundById(id: Int){}
+
 }
