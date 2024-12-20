56e7fc2d3d2a8c205b11567e74095ba676011683
Cumhur Günay <cugo2435@gmail.com>
2024-12-20 11:22:21 +0300

    Sestype made Parcelable

--- app/src/main/java/com/aecg/oyunvemuzikae/domain/SesType.kt
+++ app/src/main/java/com/aecg/oyunvemuzikae/domain/SesType.kt
@@ -1,25 +1,46 @@
-package com.aecg.oyunvemuzikae.domain
+import android.os.Parcelable
+import kotlinx.parcelize.Parcelize
 
-sealed class SesType(val displayName: String) {
+@Parcelize
+sealed class SesType(open val displayName: String) : Parcelable {
 
-    data object HAYVAN : SesType("Hayvan Sesleri")
-    data object INSAN : SesType("İnsan Sesleri")
-    data object DOGA : SesType("Doğa Sesleri")
-    data object ARAC : SesType("Araç Sesleri")
-    data object SAYI : SesType("Sayılar")
-    data object SEKIL : SesType("Geometrik Şekiller")
+    @Parcelize
+    data object HAYVAN : SesType("Hayvan Sesleri"), Parcelable
 
-    // ENSTRUMAN türü için sealed class ve alt sınıfları tanımlıyoruz
+    @Parcelize
+    data object INSAN : SesType("İnsan Sesleri"), Parcelable
+
+    @Parcelize
+    data object DOGA : SesType("Doğa Sesleri"), Parcelable
+
+    @Parcelize
+    data object ARAC : SesType("Araç Sesleri"), Parcelable
+
+    @Parcelize
+    data object SAYI : SesType("Sayılar"), Parcelable
+
+    @Parcelize
+    data object SEKIL : SesType("Geometrik Şekiller"), Parcelable
+
+    @Parcelize
     sealed class ENSTRUMAN(
-        displayName: String = "Enstrüman Sesleri",
+        override val displayName: String = "Enstrüman Sesleri",
         val displayGameHeader: String
-    ) : SesType(displayName) {
-        data object ORFF : ENSTRUMAN(displayGameHeader = "Orff")
-        data object VURMALI : ENSTRUMAN(displayGameHeader = "Vurmalı")
-        data object TELLI : ENSTRUMAN(displayGameHeader = "Telli")
-        data object UFLEMELI : ENSTRUMAN(displayGameHeader = "Üflemeli")
-        data object OTHER : ENSTRUMAN(displayGameHeader = "Diğer")
+    ) : SesType(displayName), Parcelable {
+
+        @Parcelize
+        data object ORFF : ENSTRUMAN(displayGameHeader = "Orff"), Parcelable
+
+        @Parcelize
+        data object VURMALI : ENSTRUMAN(displayGameHeader = "Vurmalı"), Parcelable
+
+        @Parcelize
+        data object TELLI : ENSTRUMAN(displayGameHeader = "Telli"), Parcelable
+
+        @Parcelize
+        data object UFLEMELI : ENSTRUMAN(displayGameHeader = "Üflemeli"), Parcelable
+
+        @Parcelize
+        data object OTHER : ENSTRUMAN(displayGameHeader = "Diğer"), Parcelable
     }
 }
-
-
